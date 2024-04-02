e89d80142d8d1b9081ce4adfc6c84f7378cfbeb8
viniciuskbaumann <161895813+viniciuskbaumann@users.noreply.github.com>
2024-04-02 20:43:47 -0300

    Delete Exercicio111.java

--- Exercicio111.java
+++
@@ -1,7 +0,0 @@
-package exercicio27032024;
-
-public class Exercicio111 {
-    public static void main(String[] args) {
-        System.out.println("O primeiro programa a gente nunca esquece! ");
-    }
-}
