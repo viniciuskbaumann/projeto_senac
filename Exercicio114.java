6e5ead3f6268da2e4e6888c9e536ebeb43a8c1e2
viniciuskbaumann <161895813+viniciuskbaumann@users.noreply.github.com>
2024-04-02 20:44:24 -0300

    Delete Exercicio114.java

--- Exercicio114.java
+++
@@ -1,7 +0,0 @@
-package exercicio27032024;
-
-public class Exercicio114 {
-    public static void main(String[] args) {
-        System.out.println("após te conhecer minha vida mudou, me senti amada novamente e abraçado por você. Tenho só que te agradecer por ser essa pessoa maravilhosa e esforçada, pra mim você é uma inspiração. ");
-    }
-}
